303ce9ed6a1bfe4017bccfdaf5e61a516532db71
Edher Jiménez <edher.jimenez@gmail.com>
2021-09-09 00:39:26 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,21 +53,4 @@ txt = st.text_area('Text to analyze', '''
      was the season of Light, it was the season of Darkness, it
      was the spring of hope, it was the winter of despair, (...)
      ''')
-st.write('Sentiment:', run_sentiment_analysis(txt))
 
-st.subheader('graph')
-
-# Add histogram data
-x1 = np.random.randn(200) - 2
-x2 = np.random.randn(200)
-x3 = np.random.randn(200) + 2
-
-# Group data together
-hist_data = [x1, x2, x3]
-
-group_labels = ['Group 1', 'Group 2', 'Group 3']
-
-# Create distplot with custom bin_size
-fig = ff.create_distplot(hist_data, group_labels, bin_size=[.1, .25, .5])
-# Plot!
-st.plotly_chart(fig, use_container_width=True)
