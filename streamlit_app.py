abed311dba13210f4ed99fc6c5fa99cc99708c04
Edher Jiménez <edher.jimenez@gmail.com>
2021-09-09 15:14:52 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -106,7 +106,7 @@ group_labels = ['Group 1', 'Group 2', 'Group 3']
 chart_data = data.groupby(['initiative', 'daily'], as_index=False).mean()
 data_sub1 = chart_data[['initiative','daily','platform_cost']]
 
-data_sub1[['daily','platform_cost']] = data_sub1[data_sub1['initiative'] == 'A lo grande']
+data_sub1['daily','platform_cost'] = data_sub1[data_sub1['initiative'] == 'A lo grande']
 st.write(data_sub1)
 
 
