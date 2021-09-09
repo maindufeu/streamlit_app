b3e0d1f922d776640ed643910bbbfcbcda023ef7
Edher Jiménez <edher.jimenez@gmail.com>
2021-09-09 17:25:21 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,7 @@ from datetime import date
 import numpy as np
 import pandas as pd
 #import plotly.figure_factory as ff
+#import graphviz as graphviz
 ######################################################################################################################################
 DATE_COLUMN = 'daily'
 DATA_URL = ('https://testingmidktbo.s3.amazonaws.com/stagging.csv')
@@ -76,7 +77,7 @@ if st.checkbox(f'Fetch {options} data'):
     response = fetch_ds(datastreams_id)
       
 #filtered_data = data[(data['daily'].dt.day).isin(days_range)]
-filtered_data = data[data['daily'].dt.day == day_to_filter[0]]
+filtered_data = data[data[DATE_COLUMN].dt.day == day_to_filter[0]]
 
 st.subheader('Campaigns at days %d' % day_to_filter)
 st.write(filtered_data)
@@ -94,16 +95,6 @@ txt = st.text_area('Text to analyze', '''
 #st.write('Sentiment:', run_sentiment_analysis(txt))
 
 st.subheader('graph')
-
-#import graphviz as graphviz
-
-# Add histogram data
-x1 = np.random.randn(200) - 2
-x2 = np.random.randn(200)
-x3 = np.random.randn(200) + 2
-
-# Group data together
-hist_data = [x1, x2, x3]
 
 group_labels = ['Group 1', 'Group 2', 'Group 3']
 
