2311a1574d21c831804d9cdecad5011b58faa541
Edher Jiménez <edher.jimenez@gmail.com>
2021-09-09 00:57:02 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,7 +72,7 @@ group_labels = ['Group 1', 'Group 2', 'Group 3']
 
 # Create distplot with custom bin_size
 
-chart_data = data.groupby([['initiative','daily']]).mean()
+chart_data = data.groupby(['initiative', 'daily']).mean()
 
 st.write(chart_data)
 
