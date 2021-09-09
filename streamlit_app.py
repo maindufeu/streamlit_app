80bdc653c4344138fd8e17449e7cfe8b68349bb1
Edher Jiménez <edher.jimenez@gmail.com>
2021-09-09 16:37:00 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -70,12 +70,12 @@ hist_values = np.histogram(data[DATE_COLUMN].dt.day, bins=31, range=(0,31))[0]
 st.bar_chart(hist_values)
 # Some number in the range 0-23
 day_to_filter = st.slider('day', 1, 31, (5, 30))
-data[DATE_COLUMN] = data[DATE_COLUMN].dt.day
+#data[DATE_COLUMN] = data[DATE_COLUMN].dt.day
 st.write('Values:', day_to_filter)
 days_range = list(range(day_to_filter[0],day_to_filter[1]))
 st.write(days_range)
 
-filtered_data = data[data['daily'].isin(days_range)]
+filtered_data = data[(data['daily'].dt.day).isin(days_range)]
 
 st.subheader('Campaigns at days %d' % day_to_filter)
 st.write(filtered_data)
