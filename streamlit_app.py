88a6c381df51d4b6034d5229312cbbd802b9d7a1
Edher Jiménez <edher.jimenez@gmail.com>
2021-09-09 13:41:15 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,15 +37,15 @@ data_load_state.text("Done! (using st.cache)")
 
 if st.checkbox('Fetch raw data'):
     for i in datastreams_id:
-    url = f'https://KTBO.datatap.adverity.com/api/datastreams/{i}/fetch_fixed/'
-    payload = json.dumps({
-      "start": "2020-08-01T00:00:00Z",
-      "end": "2021-08-06T00:00:00Z"
-    })
-    headers = {
-      'Content-Type': 'application/json',
-      'Authorization': 'Token c49e653ffa8a0c80768bbf1af0887905a56fff9b'
-    }
+        url = f'https://KTBO.datatap.adverity.com/api/datastreams/{i}/fetch_fixed/'
+        payload = json.dumps({
+        "start": "2020-08-01T00:00:00Z",
+        "end": "2021-08-06T00:00:00Z"
+        })
+        headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Token c49e653ffa8a0c80768bbf1af0887905a56fff9b'
+        }
 
     response = requests.request("POST", url, headers=headers, data=payload).json()
     st.write(response)
