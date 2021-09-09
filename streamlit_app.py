cbc4441bba95214f68464048f84077b6778dc1f7
Edher Jiménez <edher.jimenez@gmail.com>
2021-09-08 23:50:13 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,13 +28,13 @@ data_load_state = st.text('Loading data...')
 data = load_data(10000)
 data_load_state.text("Done! (using st.cache)")
 
-if st.checkbox('Show raw data'):
+if st.checkbox('Fetch raw data'):
     st.subheader('Raw data')
     st.write(data)
 
 st.subheader('Number of  by day')
 
-hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+hist_values = np.histogram(data[DATE_COLUMN].dt.day, bins=24, range=(0,24))[0]
 st.bar_chart(hist_values)
 
 # Some number in the range 0-23
