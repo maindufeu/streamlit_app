fa3b07e08903255ee5198c87b2a3c6790897b9d8
Edher Jiménez <edher.jimenez@gmail.com>
2021-09-09 13:50:05 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -98,7 +98,7 @@ chart_data = data.groupby(['initiative', 'daily'], as_index=False).mean()
 
 st.write(chart_data)
 
-#st.line_chart(chart_data)
+st.line_chart(chart_data)
 
 st.write(subprocess.call('date', shell=True))
 
