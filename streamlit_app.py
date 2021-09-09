f610e40d67475e24b26e513cf5f214e04afdd610
Edher Jiménez <edher.jimenez@gmail.com>
2021-09-08 23:38:31 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ def load_data(nrows):
     data = pd.read_csv(DATA_URL, nrows=nrows)
     lowercase = lambda x: str(x).lower()
     data.rename(lowercase, axis='columns', inplace=True)
-    data[daily] = pd.to_datetime(data[daily])
+    #data[daily] = pd.to_datetime(data[daily])
     return data
 
 data_load_state = st.text('Loading data...')
@@ -33,13 +33,13 @@ if st.checkbox('Show raw data'):
     st.write(data)
 
 st.subheader('Number of pickups by hour')
-hist_values = np.histogram(data[daily].dt.hour, bins=24, range=(0,24))[0]
-st.bar_chart(hist_values)
+#hist_values = np.histogram(data[daily].dt.hour, bins=24, range=(0,24))[0]
+#st.bar_chart(hist_values)
 
 # Some number in the range 0-23
-hour_to_filter = st.slider('hour', 0, 23, 17)
-filtered_data = data[data[daily].dt.hour == hour_to_filter]
+#hour_to_filter = st.slider('hour', 0, 23, 17)
+#filtered_data = data[data[daily].dt.hour == hour_to_filter]
 
 st.subheader('Map of all pickups at %s:00' % hour_to_filter)
-st.map(filtered_data)
+#st.map(filtered_data)
 
