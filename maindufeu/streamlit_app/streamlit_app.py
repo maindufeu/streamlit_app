9aee8579402d659cfedfb583be4c6d6869760b70
Edher Jiménez <edher.jimenez@gmail.com>
2021-09-08 23:18:08 -0500

    Update streamlit_app.py

--- maindufeu/streamlit_app/streamlit_app.py
+++ maindufeu/streamlit_app/streamlit_app.py
@@ -3,3 +3,15 @@ import streamlit as st
 # working with sample data.
 import numpy as np
 import pandas as pd
+
+
+dataframe = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20)))
+
+st.dataframe(dataframe.style.highlight_max(axis=0))
+
+import streamlit as st
+x = st.slider('x')  # 👈 this is a widget
+st.write(x, 'squared is', x * x)
+
