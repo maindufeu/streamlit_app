31eab8a459885ca6a13df7c56f77000d284822f6
Edher Jiménez <edher.jimenez@gmail.com>
2021-09-09 15:32:02 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -103,13 +103,8 @@ hist_data = [x1, x2, x3]
 group_labels = ['Group 1', 'Group 2', 'Group 3']
 
 # Create distplot with custom bin_size
-chart_data = data.groupby([['initiative','daily']], as_index=False).mean()
-data_sub1 = chart_data[['initiative','daily','platform_cost']]
+chart_data = data.groupby(['initiative','daily'], as_index=False).mean()
 st.write(chart_data)
-data_sub1 = data_sub1[data_sub1['initiative'] == 'A lo grande']
-st.write(data_sub1)
-data_sub1['daily'] = data_sub1['daily'].dt.date
-st.write(data_sub1)
 
 
 #st.line_chart(data_sub1[["daily", "platform_cost"]])
